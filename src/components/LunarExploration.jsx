c06b47d5ceec67603707c0b3b930c58130bc77bb
akshat <akshat.agrawal@w3villa.com>
2025-05-29 17:16:51 +0530

    second

--- src/components/LunarExploration.jsx
+++ src/components/LunarExploration.jsx
@@ -8,16 +8,16 @@ const LunarExploration = () => {
             <span className='text-2xl font-bold'>Discover More</span>
         </div>
         <div className='grid lg:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-3'>
-            <div className="w-full h-[480px] bg-[url('e1.webp')] bg-cover bg-center px-3 flex items-end text-white py-4">
+            <div className="w-full h-[480px] bg-[url('/e1.webp')] bg-cover bg-center px-3 flex items-end text-white py-4">
                     <span className="text-2xl font-bold">Earth's Moon </span>
             </div>
-               <div className="w-full h-[480px] bg-[url('ldep-1.webp')] bg-cover bg-center px-3 flex items-end text-white py-4">
+               <div className="w-full h-[480px] bg-[url('/ldep-1.webp')] bg-cover bg-center px-3 flex items-end text-white py-4">
                     <span className="text-2xl font-bold">Earth's Moon </span>
             </div>
-               <div className="w-full h-[480px] bg-[url('e4.png')] bg-cover bg-center px-3 flex items-end text-white py-4">
+               <div className="w-full h-[480px] bg-[url('/e4.png')] bg-cover bg-center px-3 flex items-end text-white py-4">
                     <span className="text-2xl font-bold">Earth's Moon </span>
             </div>
-               <div className="w-full h-[480px] bg-[url('e2.jpg')] bg-cover bg-center px-3 flex items-end text-white py-4">
+               <div className="w-full h-[480px] bg-[url('/e2.jpg')] bg-cover bg-center px-3 flex items-end text-white py-4">
                     <span className="text-2xl font-bold">Earth's Moon </span>
             </div>
 
